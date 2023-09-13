720d404922ae23a8e33adef8fa2dae300deef84d
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2023-09-13 19:46:09 +0600

    13.09

--- A_Comparing_Two_Long_Integers.cpp
+++ A_Comparing_Two_Long_Integers.cpp
@@ -1,60 +1,53 @@
-/*-----------------------------------------*
-*                                          * 
-*      **      ** * *    * * *   * * *     *
-*     *  *     *     *     *     *    *    *
-*    ******    ******      *     ** **     *
-*   *      *   *     *     *     *    *    *
-*  *        *  ** * *    * * *   *     *   *
-___________________________________________*/
+/*--------------------------------------------------------*
+*                                                         *
+*      **       *     *   ** ****       **      *     **  *
+*     *  *      *     *   *            *  *     *    ***  *
+*    ******     * * * *   *** ***     ******    *   *  *  *
+*   *      *    *     *         *    *      *   ***    *  *
+*  *        *   *     *   *** ***   *        *  **     *  *
+*_________________________________________________________*/
 
 #include<bits/stdc++.h>
 #define lld long long int
+#define ld long double
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define outs(x) cout << x <<" ";
+#define outn(x) cout << x << "\n";
 #define py cout<<"YES"<<"\n";
 #define pn cout<<"NO"<<"\n";
 #define nl <<"\n";
 #define srt(v) sort(v.begin(), v.end());
 #define srev(str) reverse(str.begin(), str.end());
-#define loop(i,x,n) for(lld i=x;i<n;i++)
-
-#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
-
+#define FOR(i,x,n) for(lld i=x;i<n;i++)
+#define AHSAN ios_base::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
-
-
+// const lld N= 1e7 + 10;
+// lld A[N];
 void solve()
 {
-    string s1,s2,s3,s4;
-    cin >> s1 >> s2;
-    lld ct1=0,ct2=0;
-    if(s1[0]=='0')
+    string s1,s2,s3,s4; in(s1) in(s2)
+    lld ct1=0,ct2=0,i=0,j=0;
+    while (s1[i]=='0') i++;
+    while (s2[j]=='0') j++;
+    s3=s1.substr(i,(s1.size()-i));
+    s4=s2.substr(j,(s2.size()-j));
+
+    if((s3.size()) < (s4.size())) cout << "<" nl
+    else if((s3.size()) > (s4.size())) cout << ">" nl
+    else
     {
-        for(int i=0;i<s1.size();i++)
+        if(s3==s4) cout << "=" nl
+        else
         {
-            if(s1[i]=='0')
-            ct1++;
-            else break;
+            if(s3<s4) cout << "<" nl
+            else cout << ">" nl
         }
-    }
-    for(int i=0;i<(s1.size()-ct1);i++)
-    {
-        s3[i]=s1[ct1+i];
-    }
-    if(s2[0]=='0')
-    {
-        for(int i=0;i<s2.size();i++)
-        {
-            if(s2[i]=='0')
-            ct2++;
-            else break;
-        }
-    }
-    cout << s1 << " " << s2 << s3 nl
-    cout << ct1 << " " << ct2 nl
-    
+    }    
 }
 int main()
 {
-    speed
+    AHSAN
     int t=1;
     //cin>>t;
     while (t--) solve();
--- B_Fence.cpp
+++ B_Fence.cpp
@@ -16,7 +16,7 @@
 #define outn(x) cout << x << "\n";
 #define py cout<<"YES"<<"\n";
 #define pn cout<<"NO"<<"\n";
-#define nl cout <<"\n";
+#define nl <<"\n";
 #define srt(v) sort(v.begin(), v.end());
 #define srev(str) reverse(str.begin(), str.end());
 #define FOR(i,x,n) for(lld i=x;i<n;i++)
@@ -26,31 +26,25 @@ using namespace std;
 // lld A[N];
 void solve()
 {
-    lld n,a,f; in(n)
-    vector<pair<int,int>>v;
-    FOR(i,0,n)
+    lld n,k,s=0,m,r=1; in(n) in(k)
+    lld a[n];
+    FOR(i,0,n) { in(a[i]) s+=a[i];}
+    m=s;
+	for(lld i=1;i<n-k+1;i++)
     {
-        in(a) v.push_back({a,i});
-    }
-    srt(v)
-    f=n;
-    FOR(i,0,n)
-    { 
-    v[i].first=f; 
-    f--;
-    }
-    lld r[n];
-    FOR(i,0,n)
-    {
-        r[v[i].second]=v[i].first;
-    }
-    FOR(i,0,n) outs(r[i]) nl
-
+		s=s-a[i-1]+a[k+i-1];
+		if(s<m)
+        {
+			m=s;
+			r=i+1;
+		}
+	} 
+    outn(r)
 }
 int main()
 {
     AHSAN
     int t=1;
-    cin>>t;
+    //cin>>t;
     while (t--) solve();
 }
