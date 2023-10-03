9eb89f6afd004799568edc9cfa987067746babf9
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2023-10-03 20:18:57 +0600

    3.10

--- B_Worms.cpp
+++ B_Worms.cpp
@@ -27,46 +27,26 @@
 using namespace std;
 // const lld N= 1e7 + 10;
 // lld A[N];
-
 void solve()
 {
-    lld n, m; in(n) in(m)
-    // lld gc = __gcd(n, m);
-    // n%= m;
-    // lld mm = m / gc;
-    // if ((mm & (mm - 1))) 
-    // {
-    //     outn(-1)
-    //     return;
-    // }
-    // lld ans = 0;
-    // while (n) 
-    // {
-    //     ans += n;
-    //     n <<= 1;
-    //     if (n>= m) 
-    //     {
-    //         n-=m;
-    //     }
-    // }
-    // outn(ans)
-    int x=0,ans=0;
-    while(n!=0)
+    int n; in(n)
+	vector<int> v(n);
+	FOR(i,0,n)
     {
-        n%=m;
-		ans+= n;
-		n <<= 1;
-        x++;
-        if(x==1000) break;
+        in(v[i])
+        if(i>0) v[i]= v[i]+v[i-1];
     }
-    if(x=1000 and n!=0) outn(-1)
-    else outn(ans)
-    
-} 
+	int q; in(q)
+	while (q--)
+	{
+		int x; in(x)
+		cout<<lower_bound(v.begin(), v.end(), x) - v.begin() + 1; nl
+	}
+}
 int main()
 {
     AHSAN
     int t = 1;
-    cin>>t;
+    // cin>>t;
     while (t--) solve();
 }
