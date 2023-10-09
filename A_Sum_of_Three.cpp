01f3037f9be48f4304a1a55a07e2dfcaccdba35d
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2023-10-09 23:23:43 +0600

    9.10

---
+++ A_Sum_of_Three.cpp
@@ -0,0 +1,53 @@
+/*--------------------------------------------------------*
+ *                                                         *
+ *      **       *     *   ** ****       **      *     **  *
+ *     *  *      *     *   *            *  *     *    ***  *
+ *    ******     * * * *   *** ***     ******    *   *  *  *
+ *   *      *    *     *         *    *      *   ***    *  *
+ *  *        *   *     *   *** ***   *        *  **     *  *
+ *_________________________________________________________*/
+
+#include <bits/stdc++.h>
+#define ll long long 
+#define ld long double
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define outs(x) cout << x << " ";
+#define outn(x) cout << x << "\n";
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define nl cout << "\n";
+#define vp vector<pair<ll, ll>>
+#define dsc greater<ll>()
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(v.begin(), v.end());
+#define dsrt(v) sort(v.begin(), v.end(), greater<ll>());
+#define srev(str) reverse(str.begin(), str.end());
+#define loop(i, x, n) for (ll i = x; i < n; i++)
+#define AHSAN  ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+// const ll N= 1e7 + 10;
+// ll A[N];
+void solve()
+{
+    int n; in(n)
+    if(n<=6 or n==9) pn
+    else 
+    {
+        if(n%3) 
+        {
+            py outs(1) outs(2) outs(n-3) nl    
+        }
+        else
+        {
+            py outs(1) outs(4) outs(n-5) nl
+        }
+    }
+}
+int main()
+{
+    AHSAN
+    int t = 1;
+    cin>>t;
+    while (t--) solve();
+}
