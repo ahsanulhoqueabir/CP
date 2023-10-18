7ad33994b08368392f47b76a7a756b4c6cd22b29
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2023-10-18 22:31:12 +0600

    18.10

---
+++ Maximise_Sum.cpp
@@ -0,0 +1,53 @@
+/*--------------------------------------------------------*
+ *                                                         *
+ *      **       *     *   ** ****       **      *     **  *
+ *     *  *      *     *   *            *  *     *    ***  *
+ *    ******     * * * *   *** ***     ******    *   *  *  *
+ *   *      *    *     *         *    *      *   ***    *  *
+ *  *        *   *     *   *** ***   *        *  **     *  *
+ *_________________________________________________________*/
+
+#include <bits/stdc++.h>
+#define ll long long 
+#define ld long double
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define outs(x) cout << x << " ";
+#define outn(x) cout << x << "\n";
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define nl cout << "\n";
+#define vp vector<pair<ll, ll>>
+#define dsc greater<ll>()
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(v.begin(), v.end());
+#define dsrt(v) sort(v.begin(), v.end(), greater<ll>());
+#define srev(str) reverse(str.begin(), str.end());
+#define loop(i, x, n) for (ll i = x; i < n; i++)
+#define AHSAN  ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+// const ll N= 1e7 + 10;
+// ll A[N];
+void solve()
+{
+    ll n; in(n)
+    ll a[n];
+    loop(i,0,n) in(a[i])
+    // sort(a,a+n,dsc);
+    // ll ans = a[0] + (n-1)*a[1];
+    // outn(ans)
+    ll mx=a[0];
+    ll sum=a[0];
+    loop(i,1,n)
+    {
+        mx=max(mx,a[i]);
+        sum+=mx;
+    } outn(sum)
+}
+int main()
+{
+    AHSAN
+    int t = 1;
+    cin>>t;
+    while (t--) solve();
+}
---
+++ Pizza_Cutting.cpp
@@ -0,0 +1,47 @@
+/*--------------------------------------------------------*
+ *                                                         *
+ *      **       *     *   ** ****       **      *     **  *
+ *     *  *      *     *   *            *  *     *    ***  *
+ *    ******     * * * *   *** ***     ******    *   *  *  *
+ *   *      *    *     *         *    *      *   ***    *  *
+ *  *        *   *     *   *** ***   *        *  **     *  *
+ *_________________________________________________________*/
+
+#include <bits/stdc++.h>
+#define ll long long 
+#define ld long double
+#define in(x) cin >> x;
+#define out(x) cout << x;
+#define outs(x) cout << x << " ";
+#define outn(x) cout << x << "\n";
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define nl cout << "\n";
+#define vp vector<pair<ll, ll>>
+#define dsc greater<ll>()
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(v.begin(), v.end());
+#define dsrt(v) sort(v.begin(), v.end(), greater<ll>());
+#define srev(str) reverse(str.begin(), str.end());
+#define loop(i, x, n) for (ll i = x; i < n; i++)
+#define AHSAN  ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+// const ll N= 1e7 + 10;
+// ll A[N];
+void solve()
+{
+    ll n; in(n)
+    if(n==1) py
+    else
+    {
+        if(n&1) pn
+        else py
+    }
+}
+int main()
+{
+    AHSAN
+    int t = 1;
+    cin>>t;
+    while (t--) solve();
+}
