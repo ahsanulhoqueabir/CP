4339115898b752e8d4991fbf4ff6548bdbce750b
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2023-12-30 22:53:34 +0600

    12.30

---
+++ B_Two_Divisors.cpp
@@ -0,0 +1,56 @@
+/*--------------------------------------------------------*
+ *                                                         *
+ *      **       *     *   ** ****       **      *     **  *
+ *     *  *      *     *   *            *  *     *    ***  *
+ *    ******     * * * *   *** ***     ******    *   *  *  *
+ *   *      *    *     *         *    *      *   ***    *  *
+ *  *        *   *     *   *** ***   *        *  **     *  *
+ *_________________________________________________________*/
+
+#include <bits/stdc++.h>
+#define ll long long 
+#define ld long double
+#define in(z) cin >> z;
+#define inn(z) ll z; cin >> z;
+#define sin(z) string z; cin >> z;
+#define vin(v,n) vector<ll>v(n); loop(i,0,n) in(v[i]);
+#define out(x) cout << x;
+#define outs(x) cout << x << " ";
+#define outn(x) cout << x << "\n";
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define nl cout << "\n";
+#define vp vector<pair<ll, ll>>
+#define dsc greater<ll>()
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(v.begin(), v.end());
+#define dsrt(v) sort(v.begin(), v.end(), greater<ll>());
+#define srev(str) reverse(str.begin(), str.end());
+#define loop(i, x, n) for (ll i = x; i < n; i++)
+#define AHSAN  ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+// const ll N= 1e7 + 10;
+// ll A[N];
+void solve()
+{
+  inn(a) inn(b)
+    if(b%a==0)
+    {
+        ll ans = 0;
+        ans = (b/a)*b;
+        outn(ans)
+    }
+    else
+    {
+        // ll LCM = lcm(a,b);
+        ll g= __gcd(a,b);
+        outn((a*b)/g)
+    }
+}
+int main()
+{
+    AHSAN
+    int t = 1;
+    cin>>t;
+    while (t--) solve();
+}
---
+++ D_Mathematical_Problem.cpp
@@ -0,0 +1,67 @@
+/*--------------------------------------------------------*
+ *                                                         *
+ *      **       *     *   ** ****       **      *     **  *
+ *     *  *      *     *   *            *  *     *    ***  *
+ *    ******     * * * *   *** ***     ******    *   *  *  *
+ *   *      *    *     *         *    *      *   ***    *  *
+ *  *        *   *     *   *** ***   *        *  **     *  *
+ *_________________________________________________________*/
+
+#include <bits/stdc++.h>
+#define ll long long 
+#define ld long double
+#define in(z) cin >> z;
+#define inn(z) ll z; cin >> z;
+#define sin(z) string z; cin >> z;
+#define vin(v,n) vector<ll>v(n); loop(i,0,n) in(v[i]);
+#define out(x) cout << x;
+#define outs(x) cout << x << " ";
+#define outn(x) cout << x << "\n";
+#define py cout << "YES" << "\n";
+#define pn cout << "NO" << "\n";
+#define nl cout << "\n";
+#define vp vector<pair<ll, ll>>
+#define dsc greater<ll>()
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(v.begin(), v.end());
+#define dsrt(v) sort(v.begin(), v.end(), greater<ll>());
+#define srev(str) reverse(str.begin(), str.end());
+#define loop(i, x, n) for (ll i = x; i < n; i++)
+#define AHSAN  ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+// const ll N= 1e7 + 10;
+// ll A[N];
+void solve()
+{
+    inn(n)
+    // int x= log10(n)+1;
+    // outn(x)
+    // vector<int>v;
+    // for(int i=1;;i++)
+    // {
+    //     int x= i*i;
+    //     int l = log10(x)+1;
+    //     // outn(l)
+    //     if(l==n)
+    //     {
+    //         v.push_back(x);
+    //     }
+    //     else if(l>n) break;
+    //     // if(x)
+    // } 
+    // if(v.size()>=n)
+    // {
+    // for(int i=0;i<n;i++)
+    // {
+    //     outn(v[i])
+    // }
+    // }
+    
+}
+int main()
+{
+    AHSAN
+    int t = 1;
+    cin>>t;
+    while (t--) solve();
+}
